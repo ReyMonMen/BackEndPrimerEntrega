1e419994bf8cf0e8c693f68fe422cb50f6706d15
ReyMon <rmendezelizalde@yahoo.com.ar>
2023-03-14 21:44:31 -0300

    primer entregable BackEnd

---
+++ ProductManager.js
@@ -0,0 +1,43 @@
+
+class ProductManager {
+
+    #id = 0;
+    #products = [];
+    
+
+    addProduct (product){
+
+
+        product.id = this.#id++;
+        this.#products.push(product);
+
+
+    }
+
+    getProductById(id){
+        return this.#products.find(product => product.id === id);
+    }
+    
+    getProducts(){
+
+        return this.#products;
+    }
+
+} 
+
+productoPrueba = {
+    title = "Producto de prueba",
+    description = "No sirve para un carajo",
+    price = 2000,
+    thumbnail = "Sin Imagen",
+    code = "abc123",
+    stock = 25
+};
+
+console.log(getProducts());
+
+addProduct(productoPrueba);
+
+console.log(getProducts());
+
+console.log(getProductById(1))
