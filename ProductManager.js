718e2c25b3d105d246b4e3f635cf99cf1561cca7
ReyMon <rmendezelizalde@yahoo.com.ar>
2023-03-14 22:57:36 -0300

    primer entrega final probado

--- ProductManager.js
+++ ProductManager.js
@@ -1,45 +1,78 @@
-
-class ProductManager {
-
-    #id = 0;
-    #products = [];
+class ProductManager {  
+    products = [];
+  
+    constructor () {
+      this.id = 1;
+      this.products = [];
+      
+    }
     
 
     addProduct (product){
+          
 
-        if()
+        const findCode = this.products.find(products => products.code === product.code);
 
+        if(findCode){
+            console.log(`El codigo de producto $(this.product.code) ya esta en uso`);
+        }
+        else{
+            product.id = this.id++;
+        this.products.push(product);
 
-        product.id = this.#id++;
-        this.#products.push(product);
-
-
+        }
     }
 
     getProductById(id){
-        return this.#products.find(product => product.id === id);
+      
+        const findId = this.products.find(product => product.id === id);
+      
+        if(!findId){
+          return "Producto no encontrado"
+        }
+      
+        return findId
     }
     
     getProducts(){
 
-        return this.#products;
+        return this.products;
     }
 
 } 
 
-const productoPrueba = {
-    title = "Producto de prueba",
-    description = "No sirve para un carajo",
-    price = 2000,
-    thumbnail = "Sin Imagen",
-    code = "abc123",
-    stock = 25
-};
+const productoPrueba1 = {
+    name: "Producto de prueba",
+    description: "No sirve para un carajo",
+    price: 2000,
+    thumbnail: "Sin Imagen",
+    code: "abc123",
+    stock: 25
+}
 
-console.log(getProducts());
+const productoPrueba2 = {
+    name: "Producto de prueba2",
+    description: "No sirve para un jocara",
+    price: 500,
+    thumbnail: "Sin Imagen",
+    code: "123abc",
+    stock: 12
+}
 
-addProduct(productoPrueba);
+const manager = new ProductManager();
 
-console.log(getProducts());
+console.log(manager.getProducts());
 
-console.log(getProductById(1))
+manager.addProduct(productoPrueba1);
+
+console.log(manager.getProducts());
+
+manager.addProduct(productoPrueba2);
+
+console.log(manager.getProducts());
+
+manager.addProduct(productoPrueba1);
+
+console.log(manager.getProductById(2))
+
+console.log(manager.getProductById(0))
